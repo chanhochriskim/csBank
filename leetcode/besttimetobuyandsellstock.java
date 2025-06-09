60e897a08be7f7493ce120fa6e788a1b1f31ac22
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-06-09 16:12:29 +0900

    Create besttimetobuyandsellstock.java

---
+++ leetcode/besttimetobuyandsellstock.java
@@ -0,0 +1,26 @@
+// original
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int i = 0;
+        int j = i + 1;
+
+        while (i < prices.length - 1) {
+            if (prices[i] < prices[j]) {
+                max = Math.max(prices[j] - prices[i], max);
+
+                while (j < prices.length - 1) {
+                    j++;
+                    if (prices[i] < prices[j]) {
+                        max = Math.max(prices[j] - prices[i], max);
+                    }
+                }     
+            }
+            i++;
+            j = i + 1;
+        }
+        return max;
+    }
+}
+
+// optimal O(n) sliding window method.
