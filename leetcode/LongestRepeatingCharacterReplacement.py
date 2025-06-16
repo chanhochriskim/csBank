8087ca36dfc89b44bff2f5f381d1cd7327924462
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-06-16 21:48:28 +0900

    Update LongestRepeatingCharacterReplacement.py

--- leetcode/LongestRepeatingCharacterReplacement.py
+++ leetcode/LongestRepeatingCharacterReplacement.py
@@ -12,3 +12,27 @@ class Solution:
             longest = max(longest, (r - l + 1))
 
         return longest
+
+// java version
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int left = 0;
+        int longest = 0; // longest answer
+        int maxCount = 0; // k handler.
+        int[] count = new int[26]; // alphabet counters
+
+        for (int right = 0; right < s.length(); right++) {
+            count[s.charAt(right) - 'A']++;
+            maxCount = Math.max(maxCount, count[s.charAt(right) - 'A']); 
+            // <-- most frequent alphabet. (A)
+
+            while ((right - left + 1 - maxCount) > k) { // if it goes over k limit
+                count[s.charAt(left) - 'A']--;
+                left++;
+            }
+            longest = Math.max(longest, right - left + 1); // current window
+        }
+
+        return longest;
+    }
+}
