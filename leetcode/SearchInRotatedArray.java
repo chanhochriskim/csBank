c11e02d0023bd2eb1dc5ea76af956d7cf98e554b
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-06-19 00:19:14 +0900

    Update SearchInRotatedArray.java

--- leetcode/SearchInRotatedArray.java
+++ leetcode/SearchInRotatedArray.java
@@ -1,1 +1,36 @@
-.
+// took 40min. gpt helped
+
+class Solution {
+    public int search(int[] nums, int target) {
+        // edge case handler -->
+        if (nums.length == 1 && nums[0] == target)
+            return 0;
+        // <-- edge case handler 
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2; // 7 (for --> example 1)
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+        }
+
+        return -1;
+    }
+}
