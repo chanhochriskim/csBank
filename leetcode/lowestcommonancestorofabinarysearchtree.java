3cefa5d5b818feea9d764cac2a384d5e5c335c2a
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-07-03 00:07:33 +0900

    Update lowestcommonancestorofabinarysearchtree.java

--- leetcode/lowestcommonancestorofabinarysearchtree.java
+++ leetcode/lowestcommonancestorofabinarysearchtree.java
@@ -1,4 +1,3 @@
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -11,17 +10,20 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
+        if (root == null)
+            return null;
 
-        if (root.val > p.val && root.val > q.val) {
-            // root val bigger than both of the nodes. move it to the left.
-            root = root.left;
-        } else if (root.val < p.val && root.val < q.val) {
-            // root val smaller than both. gotta move it to the right.
-            root = root.right;
-        } else {
-            // perfectly balanced. return it
-            return root;
+        while (root != null) {
+            if (root.val > p.val && root.val > q.val) {
+                // root val bigger than both of the nodes. move it to the left.
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                // root val smaller than both. gotta move it to the right.
+                root = root.right;
+            } else {
+                // perfectly balanced. return it
+                return root;
+            }
         }
         return root;
     }
