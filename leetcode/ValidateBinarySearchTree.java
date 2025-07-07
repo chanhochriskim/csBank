021eb59a902d1166f15c5477e851aa08bcc3b96f
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-07-07 23:55:52 +0900

    Update ValidateBinarySearchTree.java

--- leetcode/ValidateBinarySearchTree.java
+++ leetcode/ValidateBinarySearchTree.java
@@ -1,1 +1,27 @@
-//
+// ~18 min had to look at the prev answer. but why long would work, but not int?
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return checker(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean checker(TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if ((long) root.val <= min || (long) root.val >= max) return false;
+        return checker(root.left, min, (long) root.val) && checker(root.right, (long) root.val, max);
+    }
+}
