0bb741262450849f8392b9aac1f79efdfa5c8772
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-07-30 23:34:34 +0900

    Create CourseSchedule.py

---
+++ leetcode/CourseSchedule.py
@@ -0,0 +1,30 @@
+# neetcode solution, problem written on note, ~45min
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = { i: [] for i in range(numCourses)} # for each course, it starts with empty preReq.
+
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        
+        # visitset = all courses along curr DFS path. 
+        visitSet = set()
+        def dfs(crs):
+            if crs in visitSet: # loop detected
+                return False
+            if preMap[crs] == []: # end of path --> good
+                return True
+            visitSet.add(crs) # add this node to visitSet. 
+
+            for pre in preMap[crs]:
+                if not dfs(pre): return False
+            
+            visitSet.remove(crs)
+            preMap[crs] = []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs): return False
+
+        return True
+            
