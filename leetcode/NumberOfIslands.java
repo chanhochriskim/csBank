6df5e351a60ac148e564ecd4d2e100cd6210d728
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-09-16 10:59:32 -0400

    Update NumberOfIslands.java

--- leetcode/NumberOfIslands.java
+++ leetcode/NumberOfIslands.java
@@ -36,3 +36,39 @@ class Solution {
         dfs(grid, i, j - 1);
     }
 }
+
+
+// sep 16 -- morning prep (samsung just replied to my cold email yo!)
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0) return 0;
+        int count = 0; // ans
+        int m = grid.length;
+        int n = grid[0].length; 
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0'; // set it up as 0
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
