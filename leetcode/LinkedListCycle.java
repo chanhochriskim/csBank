f57c032ffc60050ea7580f642e6105c1b616bed6
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-09-21 10:01:28 -0400

    Update LinkedListCycle.java

--- leetcode/LinkedListCycle.java
+++ leetcode/LinkedListCycle.java
@@ -27,3 +27,33 @@ public class Solution {
         return false;
     }
 }
+
+// sep 21 -- 9:40am (before church) ~ 10:00am. 
+// you CAN NOT do slow.val == fast.val cuz of duplicates. should've done slow == fast.
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null) { // if null we know there isn't a cycle --> false.
+            if (fast.next == null) return false; 
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == null) return false; 
+            if (slow == fast) return true;
+        }
+
+        return false; // so just return false 
+    }
+}
