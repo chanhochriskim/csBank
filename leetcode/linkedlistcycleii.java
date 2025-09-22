22b1cf73f89d186b7daefdebd8b5e640f8888cc4
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-09-22 12:17:44 -0400

    Update linkedlistcycleii.java

--- leetcode/linkedlistcycleii.java
+++ leetcode/linkedlistcycleii.java
@@ -26,3 +26,49 @@ public class Solution {
         return null;
     }
 }
+
+// sep 22: 12:05 
+/* 
+<< tortoise and hare algorithm >>
+1. detect the cycle (very straightforard)
+2. algo: use another pointer (slow2) at the head. when it meets with slow, that means that's the beginning part. 
+--> t.c O(n), but Space compelxity would be O(1) since we use pointers in floyd's algo, instead of using hash table.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                break; // found the beginning part. break.
+            }
+
+        } 
+        ListNode slow2 = head; // new pointer. 
+
+        while (slow.next != null) {
+            // i mean, it's confirmed to be not null but still.
+            if (slow == slow2) return slow;
+            slow = slow.next;
+            slow2 = slow2.next;
+        }
+
+        return null;
+    }
+}
