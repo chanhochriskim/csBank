a7b3c4cc397de8bbb5089cd34f31443e52fc9b93
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-09-23 20:04:39 -0400

    Update FindMinimuminRotatedSortedArray.java

--- leetcode/FindMinimuminRotatedSortedArray.java
+++ leetcode/FindMinimuminRotatedSortedArray.java
@@ -15,3 +15,32 @@ class Solution {
         return nums[low];
     }
 }
+
+// sep 23 
+
+/* analysis:
+[3,4,5,1,2]
+get low and high = 0, nums.length - 1; 
+get mid. 
+    1. if nums[mid] > nums[high] // we know the lowest must sit within the right
+    2. --> low = mid + 1; & re-do it.
+    3. else if (nums[low] < nums[high]) // high = mid. 
+*/
+
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return nums[low]; // return the lowest value.
+    }
+}
