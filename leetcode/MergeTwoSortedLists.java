bf3632f03d1982c252d9b5ace243a4288a024d47
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-09-25 10:26:49 -0400

    Update MergeTwoSortedLists.java

--- leetcode/MergeTwoSortedLists.java
+++ leetcode/MergeTwoSortedLists.java
@@ -39,3 +39,50 @@ class Solution {
         return temp.next; // linked via Next Node.
     }
 }
+
+
+// sep 25 -- 10:23 ~ 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode(0);
+        ListNode curr = temp; 
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) { // go with list1 val
+                curr.next = list1;
+                list1 = list1.next;
+                curr = curr.next;
+            } else { // list 2 smaller. go with it.
+                curr.next = list2;
+                list2 = list2.next;
+                curr = curr.next;
+            }
+        }
+
+        // proceed with the rest if not finished
+        if (list1 != null) {  
+            curr.next = list1;
+            list1 = list1.next;
+            curr = curr.next;
+        }
+
+        if (list2 != null) {
+            curr.next = list2;
+            list2 = list2.next;
+            curr = curr.next;
+        }
+
+        return temp.next;
+    }
+}
