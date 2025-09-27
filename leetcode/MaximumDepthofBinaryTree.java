7e94cd59387894afe77149c5a54070206eb03345
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-09-27 09:39:39 -0400

    Update MaximumDepthofBinaryTree.java

--- leetcode/MaximumDepthofBinaryTree.java
+++ leetcode/MaximumDepthofBinaryTree.java
@@ -32,3 +32,46 @@ class Solution {
         return highest;
     }
 }
+
+// sep 27
+/*
+dfs(root, 1); // root, starts at count 1.
+int left, right, each increment recursively -- dfs(root.left/root.right, count++);
+--> get the maxvalue between them. math.max(left, right) for the deepest.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        return dfs(root, 1);
+    }
+
+    public int dfs(TreeNode root, int count) {
+        // left and right i guess.
+        if (root == null) {
+            return count - 1;
+        }
+
+        int left = dfs(root.left, count++);
+        int right = dfs(root.right, count++);
+        
+        return Math.max(left, right);
+    }
+}
