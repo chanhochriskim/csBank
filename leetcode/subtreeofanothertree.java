c25b3cf05cf0f702fe20451027504f4cc4ad1b65
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-09-27 10:35:16 -0400

    Update subtreeofanothertree.java

--- leetcode/subtreeofanothertree.java
+++ leetcode/subtreeofanothertree.java
@@ -27,3 +27,43 @@ class Solution {
         return dfs(root.left, subRoot.left) && dfs(root.right, subRoot.right); 
     }
 }
+
+
+// sep 27
+/*
+analysis -->
+go check if dfs with root values with subroot is true.
+    --> if so, true.
+dfs: simple recursive checker
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false; 
+        if (dfs(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean dfs(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) return true; 
+        if ((root == null || subRoot == null) || root.val != subRoot.val) return false; // it should've been root.val != subroot.val, not just root != subroot.
+
+        return dfs(root.left, subRoot.left) && dfs(root.right, subRoot.right);
+    }
+}
