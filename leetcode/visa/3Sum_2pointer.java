830d104edf6151a5cd346b380ce66d8a89af4945
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-10-22 10:34:47 -0400

    Add 3Sum solution with two-pointer approach
    Implement 3Sum solution using two-pointer technique.

---
+++ leetcode/visa/3Sum_2pointer.java
@@ -0,0 +1,35 @@
+// oct 22 - 9:45am ~ 10:10am
+// VISA
+/* analysis: 
+1. for every i, do the 2 sum method!
+2. if matches, append that to the answer.
+*/
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> set = new HashSet();
+        Arrays.sort(nums); // -4 -1 -1 0 1 2.
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue; // duplicate skip (since sorted, it checks with the adjacent values.)
+            int a = i + 1;
+            int b = nums.length - 1;
+
+            while (a < b) { // inner loop
+                int sum = nums[i] + nums[a] + nums[b]; // 
+                if (sum == 0 && !set.contains(Arrays.asList(nums[i], nums[a], nums[b]))) {
+                    set.add(Arrays.asList(nums[i], nums[a], nums[b]));
+                    a++;
+                    b--;
+                } else if (sum < 0) { // sum is something negative. bring a up.
+                    a++;
+                } else { // too big. bring b down. 
+                    b--;
+                }
+            }
+
+        }
+
+        return new ArrayList(set);
+    }
+}
