68ed08d08e70a920dce8b9c4b0d5c15a3f6c5f02
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-11-17 14:50:26 -0500

    Update top_K_frequent_elements.java

--- neetcode150/medium/top_K_frequent_elements.java
+++ neetcode150/medium/top_K_frequent_elements.java
@@ -37,3 +37,35 @@ class Solution {
         return result;
     }
 }
+
+// nov 17 -- amazon prep (went over the implementation with bucketsort and re-doing it)
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        // freq map.
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        // bucket sort
+        List<Integer>[] bucket = new List[nums.length + 1];
+        map.forEach((num, freq) -> {
+            if (bucket[freq] == null) bucket[freq] = new ArrayList();
+            bucket[freq].add(num);
+        });
+
+        // in reverse order, get the top k most frequent values.
+        List<Integer> ans = new ArrayList();
+        for (int i = bucket.length - 1; i > 0 && ans.size() < k; i--) {
+            if (bucket[i] != null) ans.addAll(bucket[i]);
+        }
+        int[] res = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
+        }
+
+        return res;
+    }
+}
