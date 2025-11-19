0a0f7e944ec54f1f8b30a63ef62b6a4a571d5b3c
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-11-19 09:40:30 -0500

    Update MergeIntervals.java

--- leetcode/MergeIntervals.java
+++ leetcode/MergeIntervals.java
@@ -60,4 +60,34 @@ class Solution {
     }
 }
 
+// nov 19 -- 9:25am ~ 9:40am
 
+/* TL DR
+- sort the intervals array based on a[0] b[0]
+- then add the first one to the ans list (list<int[]>)
+- loop over from 1 ~ end 
+    - compare curr[0] <= last[1] --> if so, update last[1] with math.max curr[1]
+    - if not, just add the curr to list.
+- return list --> list.toArray(new int[ans.size()][]);
+
+*/
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); // 1
+        List<int[]> list = new ArrayList();
+        list.add(intervals[0]); // 2
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] last = list.get(list.size() - 1);
+            int[] curr = intervals[i]; // 2 from [2,6]
+            if (curr[0] <= last[1]) { // comparing 2 from [2,6] and 3 from [1,3]
+                last[1] = Math.max(last[1], curr[1]); // update the last with curr's last
+            } else {
+                list.add(curr);
+            }
+        }
+
+        return list.toArray(new int[list.size()][]);
+    }
+}
