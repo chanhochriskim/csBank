bb1b95e2b83655e674ba0b3ccd4076a46e98ccaa
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-11-21 16:05:50 -0500

    Update NumberOfIslands.java

--- leetcode/NumberOfIslands.java
+++ leetcode/NumberOfIslands.java
@@ -29,3 +29,48 @@ class Solution {
         dfs(grid, i, j-1);
     }
 }
+--------------------
+
+    // nov 21 -- 3:45pm ~ 4:00pm --> be careful with if checker with i & j on dfs. other than that, g2g
+/*
+go over the island. if grid[i][j] == 1, conduct DFS
+*/
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0) return 0;
+
+        int[][] arr = new int[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (arr[i][j] == 1) {
+                    dfs(arr, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
+            return;
+        }
+        grid[i][j] = 0;
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
