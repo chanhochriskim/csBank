fdb9ca62c26bc5f0799b88c342fe70ea8b91c9c6
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-12-16 19:15:00 -0500

    Update jump_game.java

--- leetcode/amazon/jump_game.java
+++ leetcode/amazon/jump_game.java
@@ -1,4 +1,4 @@
-// dec 16 - 6:45pm
+// dec 16 - 6:45pm - 7:15pm
 /* Greedy in O(n) time (neetcode)
 - starting at the end, and revesre through it. shift the goal if current can make it.
 - for example, starting at the 2nd largest i-th position. there, check if from that place, we can reach to the end. if so, update goal and so on.
