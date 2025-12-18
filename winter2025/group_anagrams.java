10bb8f9938b3a175258d2d0ae91c2d88224e8fa0
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-12-18 14:21:17 -0500

    Create group_anagrams.java

---
+++ winter2025/group_anagrams.java
@@ -0,0 +1,20 @@
+// dec 18 - 2:15pm ~ 2:20pm
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap();
+
+        for (String str : strs) {
+            char[] c = str.toCharArray();
+            Arrays.sort(c);
+            String sorted = new String(c);
+
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList());
+            }
+            map.get(sorted).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
