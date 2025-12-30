452bad99c22b50aed573daafc6482352d2c1d60d
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2025-12-30 14:50:08 -0500

    Create lowest_common_ancestor_of_a_binary_search.java

---
+++ winter2025/blind75/lowest_common_ancestor_of_a_binary_search.java
@@ -0,0 +1,28 @@
+// dec 30 - 2:35pm ~ 2:50pm
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null)
+            return null;
+        while (root != null) {
+            if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else {
+                return root;
+            }
+        }
+        return null;
+    }
+}
