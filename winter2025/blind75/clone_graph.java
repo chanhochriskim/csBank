7ebfded396d7d984615cf29737f12534693a7e6a
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2026-01-06 11:20:35 -0500

    Update clone_graph.java

--- winter2025/blind75/clone_graph.java
+++ winter2025/blind75/clone_graph.java
@@ -42,3 +42,51 @@ class Solution {
         return copy;
     }
 }
+
+
+
+// jan 6 -- 11:15am ~ 11:20am
+// dfs & map (review)
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        Map<Node, Node> oldtonew = new HashMap();
+        return dfs(node, oldtonew);       
+    }
+
+    public Node dfs(Node node, Map<Node, Node> oldtonew) {
+        if (node == null) return null;
+        
+        // cycle detected
+        if (oldtonew.containsKey(node)) {
+            return oldtonew.get(node);
+        }
+
+        Node copy = new Node(node.val);
+        oldtonew.put(node, copy);
+        for (Node neighbor: node.neighbors) {
+            copy.neighbors.add(dfs(neighbor, oldtonew));
+        }
+        return copy;
+    }
+}
