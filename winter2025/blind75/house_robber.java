7b1c160542829776e34783661dd83c782b385b2e
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2026-01-08 23:29:32 -0500

    Update house_robber.java

--- winter2025/blind75/house_robber.java
+++ winter2025/blind75/house_robber.java
@@ -22,3 +22,23 @@ class Solution {
         return dp[dp.length - 1];
     }
 }
+
+
+// jan 8 -- 11:20pm ~ 11:30pm
+// classic DP. starting loop at i=2 (i=0,i=1 we already know what to do LOL)
+// --> total = math.max, return dp[last one]
+
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0]; 
+        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
+        }
+        return dp[nums.length - 1];
+    }
+}
