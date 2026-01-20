f40b64223d8c3f1a75cb5a0fa5d86af9314e0b67
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2026-01-20 00:07:43 -0500

    Update happy_number.java

--- neetcode150/easy/happy_number.java
+++ neetcode150/easy/happy_number.java
@@ -1,18 +1,26 @@
-// jan 19 - 11:45pm ~ 
+// jan 19 - 11:45pm - 12:05am
+// hashset. if we reach the same number again, that means we're stuck in a loop --> bad.
 
 class Solution {
     public boolean isHappy(int n) {
-        while () {
-            String str = String.valueOf(n); 
-            char[] ch = str.toCharArray(); // "1", "9"
-            
-            for (int i = 0; i < ch.length; i++) {
-                int a += (ch[i] - '0') * (ch[i] - '0');
-            }
-            if (a == 1) return true;
-            
+        Set<Integer> visited = new HashSet();
+        
+        while (!visited.contains(n)) {
+            visited.add(n);
+            n = helper(n);
+            if (n == 1) return true;
         }
-
         return false;
+    }
+
+    public int helper(int n) {
+        int ans = 0;
+        while (n > 0) {
+            // get the last digit, apply it into ans, then repeat.
+            int a = n % 10; // 512, 2.
+            ans += (a * a); // 4, 1, 25... 30
+            n = n / 10; // 51 now. got rid of 2.
+        }
+        return ans;
     }
 }
