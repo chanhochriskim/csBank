bc4efeb88c91cf1581e1fe6afb9a7fe86b3555c8
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2026-01-25 20:28:59 -0500

    Update min_stack.java

--- neetcode150/medium/min_stack.java
+++ neetcode150/medium/min_stack.java
@@ -1,4 +1,4 @@
-// nov 9 -- 10:45am ~ 11:10am
+p// nov 9 -- 10:45am ~ 11:10am
 /*
 to make everything to be O(1), for getmin, we will also be tracking it during push
 
@@ -49,6 +49,61 @@ class MinStack {
     
     public int getMin() {
         // retrieves the minimum element in stack in O(1).
+        return top.min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+
+// jan 25 - 8:15pm ~ 8:30pm
+// come up with own class Node (int data, int min, Node next), and go from there
+
+class Node {
+    public int data;
+    public int min;
+    public Node next;
+
+    public Node (int x, int curMin) {
+        data = x;
+        min = curMin; // to keep track of minimum value. 
+        next = null; 
+    }
+}
+
+class MinStack {
+    private Node top;
+
+    public MinStack() {
+        top = null;
+    }
+    
+    public void push(int val) {
+        int currMin = val;
+        if (top != null) currMin = Math.min(top.min, val);
+        Node temp = new Node(val, currMin);
+        temp.next = top;
+        top = temp;
+    }
+    
+    public void pop() {
+        Node temp = top;
+        top = top.next;
+        temp = null;
+    }
+    
+    public int top() {
+        return top.data;
+    }
+    
+    public int getMin() {
         return top.min;
     }
 }
