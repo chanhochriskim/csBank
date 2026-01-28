856bc453f4c7e4838d9098157e4f6b4a53df7383
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2026-01-28 10:38:33 -0500

    Update subarray_sum_equals_k.java

--- leetcode/amazon/subarray_sum_equals_k.java
+++ leetcode/amazon/subarray_sum_equals_k.java
@@ -21,3 +21,27 @@ class Solution {
         return ans;
     }
 }
+
+
+
+// jan 28 - 10:30am ~ 10:40am
+// review from yesterday 
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int ans = 0;
+        int prefix = 0;
+        HashMap<Integer, Integer> map = new HashMap();
+
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            prefix += nums[i];
+            if (map.containsKey(prefix - k)) {
+                ans += map.get(prefix - k); 
+            }
+            map.put(prefix, map.getOrDefault(prefix, 0) + 1);
+        }
+
+        return ans;
+    }
+}
