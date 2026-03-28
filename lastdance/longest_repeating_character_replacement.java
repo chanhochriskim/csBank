c1d0b893c314e2b782a57ad6db25a4c4c9da7667
Chris Kim <89702472+chanhochriskim@users.noreply.github.com>
2026-03-28 00:10:20 -0400

    Create longest_repeating_character_replacement.java

---
+++ lastdance/longest_repeating_character_replacement.java
@@ -0,0 +1,24 @@
+// mar 27 - 11:55pm ~ 
+// AABABBA  
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        if (s.length() == 1) return 1;
+        int l = 0;
+        int freq = 0;
+        int longest = 0;
+        int[] num = new int[26];
+        for (int r = 0; r < s.length(); r++) {
+            num[s.charAt(r) - 'A']++;
+            freq = Math.max(freq, num[s.charAt(r) - 'A']);
+
+            while (r - l + 1 - freq > k) {
+                num[s.charAt(l) - 'A']--;
+                l++;
+            }
+            longest = Math.max(longest, r - l + 1);
+        }
+
+        return longest;
+    }
+}
